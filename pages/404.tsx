d3bb55d8508098b120a8527839ecb568b5b682f2
superrrzzz <ubaidillahzaky11@gmail.com>
2023-01-12 06:30:47 +0700

    first commit

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const pageNotFound = () => {
+  return (
+    <div className="absolute h-full w-full bg-white top-0 z-50 flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold font-mono">PAGE NOT FOUND</h1>
+      <div className="text-4xl font-mono">
+        return to{" "}
+        <span className="underline">
+          <Link href="/">home</Link>
+        </span> page</div>
+    </div>
+  );
+};
+
+export default pageNotFound;
